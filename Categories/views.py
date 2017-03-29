4f4cb5d9d60b1621f2e0d739e4f33c673dd99aff
Zanexess <sesldark@yandex.ru>
2017-03-29 03:01:28 +0300

    for test deployment

--- Categories/views.py
+++ Categories/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from Categories.models import Category
 from Categories.serializers import CategorySerializer
 from rest_framework.decorators import api_view, permission_classes
@@ -11,25 +9,17 @@ from rest_framework.permissions import IsAuthenticated
 
 
 @csrf_exempt
-@api_view(['POST', 'GET'])
+@api_view(['GET'])
 @permission_classes((IsAuthenticated, ))
 def categories_list(request):
     if request.method == 'GET':
-        snippets = Category.objects.all()
-        serializer = CategorySerializer(snippets, many=True)
+        categories_list = Category.objects.all()
+        serializer = CategorySerializer(categories_list, many=True)
         return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CategorySerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
 
 
 @csrf_exempt
-@api_view(['POST', 'GET'])
+@api_view(['GET'])
 @permission_classes((IsAuthenticated, ))
 def category_detail(request, pk):
     try:
--- Stuff/views.py
+++ Stuff/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from Stuff.models import Stuff
 from Stuff.serializers import StuffSerializer
 from rest_framework.decorators import api_view, permission_classes
@@ -13,6 +11,6 @@ from rest_framework.permissions import IsAuthenticated
 @permission_classes((IsAuthenticated, ))
 def stuff_list(request):
     if request.method == 'GET':
-        snippets = Stuff.objects.all().filter(owner=request.user)
-        serializer = StuffSerializer(snippets, many=True)
+        stuffs = Stuff.objects.all().filter(owner=request.user)
+        serializer = StuffSerializer(stuffs, many=True)
         return JsonResponse(serializer.data, safe=False)
