44efa837af86fc02212ceeac648485271cefe4ec
Zanexess <sesldark@yandex.ru>
2017-05-28 19:39:48 +0300

    Теперь делаются миграции

--- Categories/serializers.py
+++ Categories/serializers.py
@@ -6,11 +6,12 @@ from Categories.models import Category
 class CategorySerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     category_id = serializers.CharField(required=True)
-    image_url = serializers.CharField(read_only=True, allow_null=True)
+    image_url = serializers.SerializerMethodField(read_only=True, allow_null=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=120, read_only=True)
     description = serializers.CharField(required=False, allow_blank=True, read_only=True)
     max_weight = serializers.FloatField(read_only=True, required=False)
     monthly_price = serializers.IntegerField(read_only=True, required=False)
 
     def get_image_url(self, obj):
-        return obj.image.url
+        return 'http://185.143.172.79:8000/static/' + obj.image_url.url
+
