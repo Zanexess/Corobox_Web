997dc1a6054a6c92d1114c127d3d614b092d3c59
Zanexess <sesldark@yandex.ru>
2017-05-30 22:29:29 +0300

    Заказ вещей обратно

--- Corobox/settings.py
+++ Corobox/settings.py
@@ -160,6 +160,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    '/admin/',
+    '/facebook',
+    '/rest_framework',
     '/categories/',
     '/stuff/',
 ]
